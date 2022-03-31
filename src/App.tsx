875be03d99a5bd9cfd6cb93bb8ac6c24f567f481
Jhay Rebua <hmrebua@multisyscorp.com>
2022-03-31 18:49:54 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col min-w-screen min-h-screen">React-ts-vite</div>
+  );
+}
+
+export default App;
